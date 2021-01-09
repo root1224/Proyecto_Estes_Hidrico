e1b6cdb585ec7010b67243a3093f95b6d9f39593
alan <alan@root.root.com>
2021-01-08 21:35:51 -0600

    Mejora de vista de detalle de deteccion

--- detections/views.py
+++ detections/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import CreateView, DetailView, ListView
 
 # Models
-from detections.models import Detection
+from detections.models import Detection, Note
 
 
 class IndexView(LoginRequiredMixin, ListView):
@@ -27,5 +27,12 @@ class DetectionDetailView(LoginRequiredMixin, DetailView):
     template_name = 'detections/detail.html'
     slug_field = 'name'
     slug_url_kwarg = 'name'
-    model = Detection
+    queryset = Detection.objects.all()
     context_object_name = 'detection'
+
+    def get_context_data(self, **kwargs):
+        """Add detection's notes to context."""
+        context = super().get_context_data(**kwargs)
+        detection = self.get_object()
+        context['notes'] = Note.objects.filter(note_detection=detection).order_by('-created')
+        return context
